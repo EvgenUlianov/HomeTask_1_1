53f4f6777d57cfd6ff5bd34c259043fd85d3f25c
EvgenUlianov <ulianov_ea@mail.ru>
2021-10-27 23:26:32 +0300

    task2 update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,125 @@ import java.util.*;
 import java.util.function.Consumer;
 
 public class Main {
+    static boolean needToQuit = false;
+
+    public static void main(String[] args) {
+        System.out.println("Список задач");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //List<TaskDescription> tasks = new ArrayList<>();
+        // need the type "TreeMap" because of using "lastKey()"
+        TreeMap<Integer, TaskDescription> tasks = new TreeMap<>();
+
+        Map<String, Consumer<String>> commands = new HashMap<>();
+
+        commands.put("add", (name) -> {
+            if (checkName(name)) return;
+
+            TaskDescription taskDescription = new TaskDescription(name);
+            int key;
+            if (tasks.isEmpty()){
+                key = 1;
+            } else {
+                key = tasks.lastKey() + 1;
+            }
+            tasks.put(key, taskDescription);
+        });
+
+        commands.put("print", (argument) -> {
+            // аргумент "all" воспринимается, остальное игнорируется
+            tasks.entrySet().stream()
+                    .filter((entry) -> {TaskDescription task = entry.getValue();
+                        return argument.equals("all") || !(task.isCompleted());
+                    })
+                    .forEach(Main::printTask);
+        });
+
+        commands.put("toggle", (stringNumber) -> {
+            Integer number = getNumber(stringNumber);
+            if (number == null) return;
+            TaskDescription taskDescription;
+            taskDescription = tasks.get(number);
+            if (taskDescription == null) {
+                System.out.println("идентификатор не определен");
+                return;
+            }
+            taskDescription.toggle();
+
+        });
+
+        commands.put("delete", (stringNumber) -> {
+            Integer number = getNumber(stringNumber);
+            if (number == null) return;
+            tasks.remove(number);
+        });
+
+        commands.put("edit", (argumentCommandWord) -> {
+
+            WordDelimiter wordDelimiter = new WordDelimiter(argumentCommandWord);
+            String name = wordDelimiter.secondWord;
+            if (checkName(name)) return;
+
+            String stringNumber = wordDelimiter.firstWord;
+            Integer number = getNumber(stringNumber);
+            if (number == null) return;
+
+
+            TaskDescription taskDescription;
+            taskDescription = tasks.get(number);
+            if (taskDescription == null) {
+                System.out.println("идентификатор не определен");
+                return;
+            }
+            taskDescription.setName(name);
+
+        });
+
+        commands.put("search", (substring) -> {
+            if (checkName(substring)) return;
+
+            tasks.entrySet().stream()
+                    .filter((entry) -> {TaskDescription task = entry.getValue();
+                        return task.getName().contains(substring);
+                    })
+                    .forEach(Main::printTask);
+        });
+
+
+        commands.put("quit", (argument) -> {
+            needToQuit = true;
+            System.out.println("Программа завершена");
+        });
+
+        System.out.println("Список комманд: ");
+        commands.keySet().forEach(System.out::println);
+
+        do {
+            System.out.println("type command");
+            String commandName = null;
+            try {
+                commandName = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (commandName == null)
+                break;
+
+
+            WordDelimiter wordDelimiter = new WordDelimiter(commandName);
+            String mainCommandWord = wordDelimiter.firstWord;
+            String argumentCommandWord = wordDelimiter.secondWord;
+
+            Consumer<String> command = commands.get(mainCommandWord);
+            if (command == null) {
+                System.out.println("Unknown command");
+            } else {
+                command.accept(argumentCommandWord);
+            }
+        } while (!needToQuit);
+
+    }
 
     private static class WordDelimiter {
         private final String firstWord;
@@ -23,136 +142,12 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Список задач");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        List<TaskDescription> tasks = new ArrayList<>();
-
-        Map<String, Consumer<String>> commands = new HashMap<>();
-
-        commands.put("add", (name) -> {
-            if (checkName(name)) return;
-
-            TaskDescription taskDescription = new TaskDescription(name);
-            tasks.add(taskDescription);
-        });
-
-        commands.put("print", (argument) -> {
-            // аргумент "all" воспринимается, остальное игнорируется
-            for (int i = 0; i < tasks.size(); i++) {
-                TaskDescription task = tasks.get(i);
-                if (argument.equals("all") || !(task.isCompleted()))
-                    printTask(i, task);
-            }
-        });
-
-        commands.put("toggle", (stringNumber) -> {
-            Integer number = getNumber(stringNumber);
-            if (number == null) return;
-            TaskDescription taskDescription;
-            try {
-                taskDescription = tasks.get(number - 1);
-            } catch (IndexOutOfBoundsException ex){
-                ex.printStackTrace();
-                return;
-            }
-            taskDescription.toggle();
-
-        });
-
-        commands.put("delete", (stringNumber) -> {
-            Integer number = getNumber(stringNumber);
-            if (number == null) return;
-            TaskDescription taskDescription;
-            try {
-                taskDescription = tasks.remove(number - 1);
-            } catch (IndexOutOfBoundsException ex){
-                ex.printStackTrace();
-                return;
-            }
-            taskDescription.toggle();
-
-        });
-
-        commands.put("edit", (argumentCommandWord) -> {
-
-            WordDelimiter wordDelimiter = new WordDelimiter(argumentCommandWord);
-            String name = wordDelimiter.secondWord;
-            if (checkName(name)) return;
-
-            String stringNumber = wordDelimiter.firstWord;
-            Integer number = getNumber(stringNumber);
-            if (number == null) return;
-
-
-            TaskDescription taskDescription;
-            try {
-                taskDescription = tasks.get(number - 1);
-            } catch (IndexOutOfBoundsException ex){
-                ex.printStackTrace();
-                return;
-            }
-            taskDescription.setName(name);
-
-        });
-
-        commands.put("search", (substring) -> {
-            if (checkName(substring)) return;
-
-            for (int i = 0; i < tasks.size(); i++) {
-                TaskDescription task = tasks.get(i);
-                if (task.getName().contains(substring)) {
-                    printTask(i, task);
-                }
-            }
-        });
-
-
-        final String QUIT_COMMAND = "quit";
-        commands.put(QUIT_COMMAND, (argument) -> System.out.println("Программа завершена"));
-
-        System.out.println("Список комманд: ");
-        commands.keySet().forEach(System.out::println);
-
-        while (true) {
-            System.out.println("type command");
-            String commandName = null;
-            try {
-                commandName = reader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            if (commandName == null)
-                break;
-
-
-
-            WordDelimiter wordDelimiter = new WordDelimiter(commandName);
-            String mainCommandWord = wordDelimiter.firstWord;
-            String argumentCommandWord = wordDelimiter.secondWord;
-
-            Consumer<String> command  = commands.get(mainCommandWord);
-            if (command == null)
-                break;
-            command.accept(argumentCommandWord);
-            if (mainCommandWord.equals(QUIT_COMMAND))
-                break;
-        }
-
-    }
-
     private static boolean checkName (String name) {
         if (name.length() == 0) {
             System.out.println("Не указано наименование");
             return true;
         }
         return false;
-    }
-
-    private static void printTask(int i, TaskDescription task) {
-        System.out.printf("%d. [%s] %s%n", i + 1, (task.isCompleted() ? "x" : " "), task.getName());
     }
 
     private static Integer getNumber(String stringNumber) {
@@ -169,6 +164,12 @@ public class Main {
             return null;
         }
         return number;
+    }
+
+    private static void printTask(Map.Entry<Integer, TaskDescription> entry) {
+        Integer key = entry.getKey();
+        TaskDescription task = entry.getValue();
+        System.out.printf("%d. [%s] %s%n", key, (task.isCompleted() ? "x" : " "), task.getName());
     }
 
 
